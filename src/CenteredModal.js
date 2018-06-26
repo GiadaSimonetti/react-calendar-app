6d40d14b49bf7b86c5333d2f8992580d71f35efd
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-06-26 11:38:24 +0100

    Create a CenteredModal component

---
+++ src/CenteredModal.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Modal from 'react-responsive-modal';
+
+class CenteredModal extends React.Component {
+  state = {
+    open: false,
+  };
+
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+
+  render() {
+    const { open } = this.state;
+    return (
+      <div className="modal">
+        <h4>Centered modal</h4>
+        <button className="btn btn-action" onClick={this.onOpenModal}>
+          Open
+        </button>{' '}
+        <div className="modal-wrapper">
+          <Modal open={open} onClose={this.onCloseModal} center>
+          <div className="modal-header">
+          <h2>Calendar</h2>
+          </div>
+          <div className="modal-content">
+
+          </div>
+          <div className="modal-footer">
+          <p>I am the footer</p>
+          </div>
+          </Modal>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CenteredModal;
